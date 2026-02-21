8fe75756921bcd63ba48978fc641bcd474f01260
TaBiZoo <rafik-2015@live.fr>
2026-02-21 04:06:11 +0100

    fixed login ui

--- WPF APP/Views/LoginWindow.xaml.cs
+++ WPF APP/Views/LoginWindow.xaml.cs
@@ -37,5 +37,37 @@ namespace WPF_APP.Views
         {
             WindowHelper.CloseApp();
         }
+
+        private void txtUserName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtUserName.Text == "Nom d'utilisateur")
+            {
+                txtUserName.Text = "";
+                txtUserName.Opacity = 1; 
+            }
+        }
+
+        private void txtUserName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUserName.Text))
+            {
+                txtUserName.Text = "Nom d'utilisateur";
+                txtUserName.Opacity = 0.5; 
+            }
+        }
+
+
+        private void txtPassWord_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            // Si l'utilisateur a tapé quelque chose, on cache le texte "Mot de passe"
+            if (txtPassWord.Password.Length > 0)
+            {
+                textPasswordPlaceholder.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPasswordPlaceholder.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
