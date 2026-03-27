ac295cb07e233b7c1a1603a8ce28055673691950
TaBiZoo <rafik-2015@live.fr>
2026-03-28 00:19:19 +0100

    Fix: Renamed project folder to ClubManagementSystem and updated SLN

---
+++ ClubManagementSystem/Helpers/AttendanceToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ClubManagementSystem.Helpers
+{
+    public class AttendanceToColorConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // On vérifie si la valeur est un nombre (pour le taux de présence)
+            if (value is int rate || (value != null && int.TryParse(value.ToString(), out rate)))
+            {
+                if (rate >= 80) return new SolidColorBrush(Colors.Green);      // Très présent
+                if (rate >= 50) return new SolidColorBrush(Colors.Orange);     // Moyen
+                return new SolidColorBrush(Colors.Red);                        // Peu présent
+            }
+
+            return new SolidColorBrush(Colors.Gray);
+        }
+
+    }
+}
