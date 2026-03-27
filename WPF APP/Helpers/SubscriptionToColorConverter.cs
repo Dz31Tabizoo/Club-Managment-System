5b548a35a310ba4209ca82faa73bc0d71b1925b7
TaBiZoo <rafik-2015@live.fr>
2026-03-27 23:55:31 +0100

    backup before renaming a folder

---
+++ WPF APP/Helpers/SubscriptionToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ClubManagementSystem.Helpers
+{
+    public class SubscriptionToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int rate || (value != null && int.TryParse(value.ToString(), out rate)))
+            {
+                if (rate >= 80) return new SolidColorBrush(Colors.Green);
+                if (rate >= 50) return new SolidColorBrush(Colors.Orange);
+                return new SolidColorBrush(Colors.Red);
+            }
+            return new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
+
